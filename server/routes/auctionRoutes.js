bf5dff4f96b9115354a39249d70f23ac1f3b7f52
Mushtaba <MushtabaA1234@gmail.com>
2021-11-23 22:30:04 -0700

    Created endpoints for auction that joins with both User and Vehicle

--- server/routes/auctionRoutes.js
+++ server/routes/auctionRoutes.js
@@ -21,6 +21,64 @@ router.get(
   })
 );
 
+// @desc    Fetch auctions joined with User
+// @route   GET /api/auctions/joinuser
+// @access  Public
+router.get(
+  '/joinuser',
+  asyncHandler(async (req, res) => {
+    Auction.find().populate('seller')
+      .exec(function (err, auction) {
+        if (err) return handleError(err);
+        res.send(auction);
+      });
+  })
+);
+
+// @desc    Fetch auctions joined with Vehicle
+// @route   GET /api/auctions/joinvehicle
+// @access  Public
+router.get(
+  '/joinvehicle',
+  asyncHandler(async (req, res) => {
+    Auction.find().populate('vehicle')
+      .exec(function (err, auction) {
+        if (err) return handleError(err);
+        res.send(auction);
+      });
+  })
+);
+
+// @desc    Fetch auctions joined with User and Vehicle
+// @route   GET /api/auctions/joinboth
+// @access  Public
+router.get(
+  '/joinboth',
+  asyncHandler(async (req, res) => {
+    Auction.find().populate('seller').populate('vehicle')
+      .exec(function (err, auction) {
+        if (err) return handleError(err);
+        res.send(auction);
+      });
+  })
+);
+
+// @desc    Fetch auctions by id
+// @route   GET /api/auctions/:id
+// @access  Public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const auctions = await Auction.findById(req.params.id);
+
+    if (auctions) {
+      res.send(auctions);
+    } else {
+      res.send({ errMessage: 'Auction not found' });
+    }
+  })
+);
+
 // @desc    Posting an auction
 // @route   POST /api/auctions
 // @access  Public
