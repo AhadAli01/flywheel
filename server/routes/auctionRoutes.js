66bde4545de480ca2355cbca2a016048b49f1069
Mushtaba <MushtabaA1234@gmail.com>
2021-12-12 12:24:34 -0700

    updated auctionRoutes backend

--- server/routes/auctionRoutes.js
+++ server/routes/auctionRoutes.js
@@ -16,45 +16,6 @@ import {
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    const auctions = await Auction.find({});
-    res.json(auctions);
-  })
-);
-
-// @desc    Fetch auctions joined with user
-// @route   GET /api/auctions/joinuser
-// @access  Public
-router.get(
-  '/joinuser',
-  asyncHandler(async (req, res) => {
-    Auction.find().populate('seller')
-      .exec(function (err, auction) {
-        if (err) return handleError(err);
-        res.send(auction);
-      });
-  })
-);
-
-// @desc    Fetch auctions joined with vehicle
-// @route   GET /api/auctions/joinvehicle
-// @access  Public
-router.get(
-  '/joinvehicle',
-  asyncHandler(async (req, res) => {
-    Auction.find().populate('vehicle')
-      .exec(function (err, auction) {
-        if (err) return handleError(err);
-        res.send(auction);
-      });
-  })
-);
-
-// @desc    Fetch auctions joined with user and vehicle
-// @route   GET /api/auctions/joinboth
-// @access  Public
-router.get(
-  '/joinboth',
-  asyncHandler(async (req, res) => {
     Auction.find().populate('seller').populate('vehicle')
       .exec(function (err, auction) {
         if (err) return handleError(err);
@@ -69,7 +30,7 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    const auctions = await Auction.findById(req.params.id);
+    const auctions = await Auction.findById(req.params.id).populate('seller').populate('vehicle');
 
     if (auctions) {
       res.send(auctions);
