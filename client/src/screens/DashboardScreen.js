000510a96f425530c233364a3e5fd78fe67967c6
Parbir <parbir.lehal@gmail.com>
2021-12-15 15:24:05 -0700

    Updated post request on dashboardscreen

--- client/src/screens/DashboardScreen.js
+++ client/src/screens/DashboardScreen.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 
 const data = localStorage.getItem("userData");
-const object = JSON.parse(data);
+const user = JSON.parse(data);
 
 const Dashboard = ({auth}) => {
   const [profile, setProfile] = useState([]);
@@ -29,22 +29,41 @@ const Dashboard = ({auth}) => {
   else{
 
 
-  const update = (event) => {
+  const update = async (event) => {
     event.preventDefault();
-    axios
-      .post('/api/users/dashboard', {
-        user: object._id,
-        address: newAddress,
-        phone: newPhone,
-      })
-      .then((response) => {
-        if (response.data.errMessage) {
-          alert(response.data.errMessage);
-        } else {
-          sessionStorage.setItem('profileData', JSON.stringify(response.data));
-          window.location.reload();
-        }
-      });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    let variable = {};
+    variable = {
+      user: user._id,
+      address: newAddress,
+      phone: newPhone,
+    };
+    try {
+      const { data } = await axios.post(
+        `/api/users/profile`,
+        variable,
+        config,
+      )
+      alert("Profile updated")
+      sessionStorage.setItem('profileData', JSON.stringify(data));
+      window.location.reload();
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+
+
+      // .then((response) => {
+      //   if (response.data.errMessage) {
+      //     alert(response.data.errMessage);
+      //   } else {
+      //     sessionStorage.setItem('profileData', JSON.stringify(response.data));
+      //     window.location.reload();
+      //   }
+      // });
   };
 
   return (
@@ -59,7 +78,7 @@ const Dashboard = ({auth}) => {
             onChange={(event) => {
               setNewName(event.target.value);
             }}
-            placeholder={object.name}
+            placeholder={user.name}
           />
           <input
             className='parbirInputs'
@@ -68,7 +87,7 @@ const Dashboard = ({auth}) => {
             onChange={(event) => {
               setNewEmail(event.target.value);
             }}
-            placeholder={object.email}
+            placeholder={user.email}
           />
           <input
             className='parbirInputs'
