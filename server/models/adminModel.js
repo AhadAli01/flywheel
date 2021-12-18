e40ff6fcca81e875ae9a57509082d72762128749
AhadAli01 <ahadali101aa@gmail.com>
2021-12-17 23:30:27 -0700

    admin format

--- server/models/adminModel.js
+++ server/models/adminModel.js
@@ -1,31 +1,27 @@
 import mongoose from 'mongoose';
 
-const adminSchema = mongoose.Schema(
-    {
-        user: {
-            type: mongoose.Schema.Types.ObjectId,
-            required: true,
-            ref: 'User', // MongoDB/Mongoose equivalent to instantiating a foreign key in SQL
-          },
+const adminSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User', // MongoDB/Mongoose equivalent to instantiating a foreign key in SQL
+  },
 
-        name: {
-            type: String,
-            required: true,
-          },
+  name: {
+    type: String,
+    required: true,
+  },
 
-          address: { type: String},
+  address: { type: String },
 
-          position: { type: String},
+  position: { type: String },
 
-          password: { type: String},
-          
-          phone: { type: String },
+  password: { type: String },
 
-          officeLoc: { type: String},
+  phone: { type: String },
 
-
-    }
-);
+  officeLoc: { type: String },
+});
 
 const Admin = mongoose.model('Admin', adminSchema);
 export { Admin };
